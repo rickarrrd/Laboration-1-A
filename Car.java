c02dde97590548d8ce68bdd44481b57e1de35570
rickarrrd <74190766+rickarrrd@users.noreply.github.com>
2020-11-09 14:30:43 +0100

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,65 @@
+package Cars;
+import java.awt.Color;
+
+public abstract class Car implements Movable{
+	
+    protected int nrDoors; // Number of doors on the car
+    protected double enginePower; // Engine power of the car
+    protected double currentSpeed; // The current speed of the car
+    protected Color color; // Color of the car
+    public String modelName; // The car model name
+	
+	// public Car() {}
+    
+    public void move() {
+    	
+    }
+	public void turnLeft() {
+		
+	}
+	public void turnRight() {
+		
+	}
+	
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+	    color = clr;
+    }
+
+    public void startEngine(){
+	    currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+	    currentSpeed = 0;
+    }
+    
+    public abstract void incrementSpeed(double amount);
+
+    public abstract void decrementSpeed(double amount);
+    
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
+
+}
