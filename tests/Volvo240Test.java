41b97d8c1dedbcbcf5f78130807e30442b3f9f84
Rayinfluense <samuel125807@hotmail.com>
2020-11-11 11:46:31 +0100

    tests for volvo240, some tests left todo

--- tests/Volvo240Test.java
+++ tests/Volvo240Test.java
@@ -3,10 +3,46 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Volvo240Test {
-
+    double delta=0.01;
+    double engineOffSpeed=0; //Newly created volvo currently starts with engine off speed, ie currentSpeed=0
+    double startEngineSpeed=0.1;
+    double trimFactor = 1.25;
+    double enginePower= 100;
+    double incrementTestAmount=0.5;
     @Test
-    public void speedFactor(){
+    public void getSpeedFactor(){
         Volvo240 volvo240test = new Volvo240();
-        assertEquals(1.25, volvo240test.speedFactor(), 0.01);
+        assertEquals(trimFactor*enginePower*0.01, volvo240test.getSpeedFactor(), delta);
+    }
+
+    public void getCurrentSpeed(){
+        Volvo240 volvo240test = new Volvo240();
+        assertEquals(engineOffSpeed, volvo240test.getCurrentSpeed(), delta);
+
+        volvo240test.startEngine();
+        assertEquals(startEngineSpeed,volvo240test.getCurrentSpeed(),delta);
+
+        volvo240test.incrementSpeed(incrementTestAmount);
+        assertEquals(trimFactor*incrementTestAmount+startEngineSpeed,volvo240test.getCurrentSpeed(),delta);
+
+    }
+
+    public void incrementSpeed(){
+        Volvo240 volvo240test = new Volvo240();
+
+        volvo240test.incrementSpeed(incrementTestAmount);
+        assertEquals(engineOffSpeed + volvo240test.getSpeedFactor()*incrementTestAmount,
+                     volvo240test.getCurrentSpeed(),delta);
+
+        volvo240test.incrementSpeed(enginePower);
+        assertEquals(100, volvo240test.getCurrentSpeed(),delta);
+
+    }
+
+    public void decrementSpeed(){
+        Volvo240 volvo240test = new Volvo240();
+
+        volvo240test.decrementSpeed(incrementTestAmount);
+        assertEquals(-0.625,volvo240test.getCurrentSpeed(),delta);
     }
 }
