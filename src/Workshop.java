d5ba9a908a33cc8e24b31c9907c083b539a20c6f
Rayinfluense <samuel125807@hotmail.com>
2020-11-18 19:59:47 +0100

    Constructors for workshop

--- src/Workshop.java
+++ src/Workshop.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class Workshop {
+
+    private VehicleCarrierBedHelper vehicleCarrierBedHelper;
+    private int vehicleMaxAmount;
+    ArrayList<String> whitelistedCarModels;
+    public Workshop(int vehicleMaxAmount, ArrayList<String> whitelistedCarModels){
+        this.vehicleCarrierBedHelper = new VehicleCarrierBedHelper(this);
+        this.vehicleMaxAmount=vehicleCarrierBedHelper.setVehicleMaxAmount(vehicleMaxAmount);
+        this.whitelistedCarModels=whitelistedCarModels
+    }
+
 
 }
