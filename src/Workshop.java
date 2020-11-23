6412baa87a608c3bd0a0356bc813ae393f471ac9
premell <elmer.lingestal@live.se>
2020-11-23 21:54:52 +0100

    added comments to workshop

--- src/Workshop.java
+++ src/Workshop.java
@@ -12,6 +12,15 @@ public class Workshop implements IStructureWithCarrierBed{
     private double yCord;
     private int direction;
 
+
+    /**
+     * Sets the parameters of the Workshop
+     * @param vehicleMaxAmount max capacity of cars
+     * @param whitelistedCarModels which car models are allowed
+     * @param xCord The x coordinate of the workshop
+     * @param yCord The y coordinate of the workshop
+     * @param direction
+     */
     public Workshop(int vehicleMaxAmount, ArrayList<Vehicle> whitelistedCarModels, int xCord, int yCord, int direction){
         this.carCarrierBed= new CarCarrierBed( this,vehicleMaxAmount);
         this.vehicleCarrierBedHelper = new VehicleCarrierBedHelper(carCarrierBed, this);
@@ -22,14 +31,22 @@ public class Workshop implements IStructureWithCarrierBed{
         this.direction=direction;
     }
 
-
-
+    /**
+     * Load a car into the workshop
+     * @param vehicle the car to be loaded
+     */
     public void loadVehicle(Vehicle vehicle){
         if(this.checkIfInWhitelist(whitelistedCarModels,vehicle)) {
             carCarrierBed.loadVehicle(vehicle);
         }
     }
 
+    /**
+     * Check wheter or not a car is allowed
+     * @param whitelistedCarModels cars allowed
+     * @param vehicleToBeChecked the car to be checked
+     * @return wheter or not the car is allowed or not
+     */
     private boolean checkIfInWhitelist(ArrayList<Vehicle> whitelistedCarModels, Vehicle vehicleToBeChecked){
         for(Vehicle whitelistedCarModel : whitelistedCarModels){
             if(vehicleToBeChecked.getClass() == whitelistedCarModel.getClass()){
@@ -39,22 +56,34 @@ public class Workshop implements IStructureWithCarrierBed{
         return false;
     }
 
+    /**
+     * Release a car from the workshop
+     */
     public void unloadVehicle() {
         carCarrierBed.unloadVehicle();
     }
 
+    /**
+     * @return the x coordinate of the workshop
+     */
     public double getXcord(){
         return xCord;
     }
 
+    /**
+     * @return the y coordinate of the workshop
+     */
     public double getYcord(){
         return yCord;
     }
 
+    /**
+     * @return the direction of the workshop
+     */
     public double getDirection(){
         return direction;
     }
-
+//Ugh fy fan, hatar att ha den här här
     public double getCurrentSpeed(){
         return 0;
     }
@@ -67,10 +96,17 @@ public class Workshop implements IStructureWithCarrierBed{
         return vehicleMaxAmount;
     }
 
+    /**
+     * @return list of all stored cars
+     */
     public ArrayList<Vehicle> getCarriedVehicles(){
         return carCarrierBed.getCarriedVehicles();
     }
 
+    /**
+     * Add a vehicle to the workshop
+     * @param vehicle the vehicle to be added
+     */
     public void addVehicle(Vehicle vehicle){
         carCarrierBed.addVehicle(vehicle);
     }
